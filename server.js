eecbbb7384b3a238cd9305ed1a54514455d03a90
gimhantharuke456 <gimhantharuke456@gmail.com>
2024-04-17 21:55:33 +0530

    test commit

--- server.js
+++ server.js
@@ -26,7 +26,7 @@ app.use("/users", userRoutes);
 const PORT = process.env.PORT || 4000;
 
 app.listen(PORT, () => {
-  console.log(`Server is running on port in ${PORT}`);
+  console.log(`Server is running on port  ${PORT}`);
 });
 
 module.exports = app;
